d2bd37877a7d02beccff1600ff1fd8de6dea0535
Your Name <you@example.com>
2019-11-24 10:47:26 +0800

    发布文章已完成

--- web_back/js/config.js
+++ web_back/js/config.js
@@ -10,7 +10,9 @@ var APIURLS = {
   category_add: baseUrl + 'admin/category_add',
   category_edit: baseUrl + 'admin/category_edit',
   article_show: baseUrl + 'admin/search',
-  article_del: baseUrl + 'admin/article_delete'
+  article_del: baseUrl + 'admin/article_delete',
+  article_add: baseUrl + 'admin/article_publish',
+
 
 
 }
--- web_back/utils/article.js
+++ web_back/utils/article.js
@@ -1,4 +1,5 @@
 var article = {
+  // 获取文章
   show: function(curPage, type, state, callback) {
     $.get(APIURLS.article_show, {
       page: curPage,
@@ -8,6 +9,7 @@ var article = {
       callback(res);
     })
   },
+  // 删除文章
   del: function(id, callback) {
     $.get(APIURLS.article_del, {
       id: id
@@ -15,5 +17,19 @@ var article = {
       callback(res)
     })
 
+  },
+  // 添加文章
+  add: function(fd, callback) {
+    $.ajax({
+      url: APIURLS.article_add,
+      type: 'post',
+      data: fd,
+      processData: false, // 不要让jquery去处理formdata数据
+      contentType: false, // 不要使用默认的请求头
+      success: function(res) {
+        callback(res)
+
+      }
+    })
   }
 }
